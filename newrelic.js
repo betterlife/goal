c820657e54439cf8e833ff54b7cd6c0e1fba78fe
Lawrence as Developer <smartlitchi@gmail.com>
2014-01-29 22:59:28 +0800

    Change app name back

--- newrelic.js
+++ newrelic.js
@@ -8,7 +8,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name : ['Life Better Goal Staging'],
+  app_name : ['Life Better Goal'],
   /**
    * Your New Relic license key.
    */
