b914ab2394aa37c17ef3ca1b86b49601a78bd522
Lawrence as Developer <smartlitchi@gmail.com>
2014-03-31 09:19:39 +0800

    Fix #30 again

--- public/javascripts/controllers/nav.js
+++ public/javascripts/controllers/nav.js
@@ -1,7 +1,10 @@
+/*jshint global: angluar*/
 "use strict";
 
 var navController = function($scope, $http, $location, $modal, $routeParams) {
-
+    $scope.isActive = function(route) {
+      return route === $location.path();        
+    };
 };
 
 navController.$inject = ['$scope', '$http', '$location', '$modal', '$routeParams'];
